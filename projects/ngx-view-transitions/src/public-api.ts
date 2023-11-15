3a028d9f0052416560eb4a9d2d8c784d261c0724
Monte Hellawell <monte@montudor.com>
2023-11-15 10:48:52 +0000

    fix: remove unused exports

--- projects/ngx-view-transitions/src/public-api.ts
+++ projects/ngx-view-transitions/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ngx-view-transitions
  */
 
-export * from './lib/ngx-view-transitions.service'
-export * from './lib/ngx-view-transitions.component'
 export * from './lib/ngx-view-transitions.module'
 export * from './lib/directives/transition-start.directive'
 export * from './lib/directives/transition-end.directive'
