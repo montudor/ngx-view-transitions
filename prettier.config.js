f43a5e6cc91aa372cbefd00fd66920adcbf3c6cc
Monte Hellawell <monte@montudor.com>
2023-11-15 08:49:54 +0000

    chore: add linting to project

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  printWidth: 120
+}
